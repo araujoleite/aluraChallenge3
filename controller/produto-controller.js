4781df64f8c104c4f8001a6c80d2bde020f6b3b3
Alan Araujo Leite <alansl@outlook.com>
2022-04-19 07:56:32 -0300

    Dynamic infos in produto.html

--- controller/produto-controller.js
+++ controller/produto-controller.js
@@ -6,6 +6,16 @@ import { clientService } from "../service/client-service.js";
 
     const item = await clientService.itemInfos(id)
 
-    console.log(item)
-    
+    const productImage = document.querySelector('[data-productImg]')
+    productImage.style = `background: url(${item.image}) center; background-size: cover`
+
+    const productDetail = document.querySelector('[data-productDetail]')
+    productDetail.innerHTML = `
+        <div class="product__details">
+            <h1 class="product__name">${item.name}</h1>
+            <p class="product__price">R$${item.price}</p>
+            <p class="product__info">${item.description}</p>
+        </div>
+    `
+    console.log(item)    
 })()
